e847d8846d26a6557dac05f418d3d64721c01158
unknown <atauraahmad@gmail.com>
2025-07-19 23:27:02 +0100

    began the js part

---
+++ book_library/script.js
@@ -0,0 +1,13 @@
+const addBookButton = document.querySelector('.add-book-button');
+const addBook = document.querySelector('.add');
+const title = document.querySelector('#title');
+const read = document.querySelector('#read');
+const author = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+
+
+const bookList = document.querySelector('.booklist');
+
+{/* <p class="book-item"><span>Enjoy your life by Al-areefy with 452 pages and not read </span> <i */ }
+// class="fa fa-times" aria-hidden="true"></i> </p>
+
